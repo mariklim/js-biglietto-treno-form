7563f619d2e2c93a861a61ac737299932ed884be
mariklim <maria.klimenko889@gmail.com>
2021-08-30 16:12:58 +0200

    add var

--- js/script.js
+++ js/script.js
@@ -4,3 +4,19 @@
 // va applicato uno sconto del 20% per i minorenni
 // va applicato uno sconto del 40% per gli over 65.
 // Il recap dei dati e l'output del prezzo finale va stampato in pagina (formattato con massimo due decimali, per indicare centesimi sul prezzo). Questo richiederà un minimo di ricerca.
+
+// 1. Salvo i dati del passaggero nelle var: nome, km e ageGroup
+// 2. Creo una var price dove calcolo il prezzo in base ai km (0.21 € al km)
+// 3. Creo una condizione if per applicare lo sconto
+// 4. Creo var per la carrozza e il numero di pronotazione cge genera un mumero random
+// 5. Creo una funzione per evento click che stampa i dati attraverso nelle caselle giuste document.getElementById(id).innerHTML =
+// 6. Creo una funzione per evento clck -cancel che unnulla tutti i dati inseriti
+
+var nomePassanger = document.getElementById("name").value;
+// "name" è ID che abbiamo indicato nella ticket-form
+var km = parseInt(document.getElementById("km").value);
+var ageGroup = document.getElementById("age-group").value;
+
+console.log(nomePassanger);
+console.log(km);
+console.log(ageGroup);
